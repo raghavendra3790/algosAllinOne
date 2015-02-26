3a4bbb0b80cd79c16b391e155bd2cd2ace62c649
raghavendra.3790 <raghavendra.3790@gmail.com>
2015-02-25 17:45:28 -0800

    Graph Project, Graph API and Graph Search Interface

---
+++ Graphs/IGraphSearch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Graphs
+{
+    /// <summary>
+    /// Interface that defines BFS and DFS
+    /// </summary>
+    public interface IGraphSearch
+    {
+        /// <summary>
+        /// Is there a path from source to V?
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        bool HasPath(int v);
+
+        /// <summary>
+        /// Get the path from Source to V
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        List<int> Path(int v);
+    }
+}
---
+++ Graphs/UndirectedGraph.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Graphs
+{
+    public class UndirectedGraph
+    {
+        public UndirectedGraph(int vertices)
+        {
+            Vertices = vertices;
+            _adj = new List<int>[vertices];
+            for (int i = 0; i < vertices; i++)
+            {
+                _adj[i] = new List<int>();
+            }
+        }
+
+        private List<int>[] _adj; 
+        public int Vertices { get; private set; }
+
+        public void AddEdge(int v, int w)
+        {
+            // Validate the vertices
+            CheckRangeAndThrow(v);
+            CheckRangeAndThrow(w);
+
+            // Add mutual edges. Since it is undirected adding one edge makes both adjacent
+            this._adj[v].Add(w);
+            this._adj[w].Add(v);
+        }
+
+        public List<int> Adjacent(int v)
+        {
+            CheckRangeAndThrow(v);
+
+            return this._adj[v];
+        }
+
+        private void CheckRangeAndThrow(int v)
+        {
+            if (v < 0 || v > Vertices - 1)
+            {
+                throw new ArgumentException("V");
+            }
+        }
+    }
+}
