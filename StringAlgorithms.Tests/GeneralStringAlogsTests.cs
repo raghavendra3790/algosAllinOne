ad7f2fb10a9e7e0b2c671a3abb0a59ae31d2a862
raghavendra.3790 <raghavendra.3790@gmail.com>
2015-03-02 22:40:31 -0800

    Geenral String Algorithm with Tests

---
+++ StringAlgorithms.Tests/GeneralStringAlogsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringAlgorithms.Tests
+{
+    [TestClass]
+    public class GeneralStringAlogsTests
+    {
+        [TestMethod]
+        public void TestIsAnagram()
+        {
+            Assert.IsTrue(GeneralStringAlgos.isAnagram("hello", "hello"));
+        }
+
+        [TestMethod]
+        public void TestIsAnagramFalse()
+        {
+            Assert.IsFalse(GeneralStringAlgos.isAnagram("alpha", "alphs"));
+        }
+
+        [TestMethod]
+        public void TestIsPalindrome()
+        {
+            Assert.IsTrue(GeneralStringAlgos.isPalindrome("madam"));
+            Assert.IsFalse(GeneralStringAlgos.isPalindrome("general"));
+        }
+    }
+}
---
+++ StringAlgorithms/GeneralStringAlgos.cs
@@ -0,0 +1,42 @@
+﻿namespace StringAlgorithms
+{
+    public class GeneralStringAlgos
+    {
+        public static bool isAnagram(string s1, string s2)
+        {
+            // if length of strings not equal then they cant be anagrams.
+            if (s1.Length != s2.Length)
+                return false;
+
+            int[] alpha = new int[256];
+
+            for (int i = 0; i < s1.Length; i++)
+            {
+                alpha[s1[i]]++;
+                alpha[s2[i]]--;
+            }
+
+            foreach (int i in alpha)
+                if (i != 0) return false;
+
+            return true;
+        }
+
+        public static bool isPalindrome(string s)
+        {
+            int start = 0;
+            int end = s.Length - 1;
+
+            while (start < end)
+            {
+                if (s[start] != s[end])
+                    return false;
+
+                start++;
+                end--;
+            }
+
+            return true;
+        }
+    }
+}
